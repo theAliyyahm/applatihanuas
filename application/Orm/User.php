5471518fb4126f7aca2dc32d8e4922544f247260
theAliyyahm <maftukhatulaliyyah@gmail.com>
2023-07-04 22:01:48 +0700

    first commit

---
+++ application/Orm/User.php
@@ -0,0 +1,14 @@
+<?php
+namespace Orm;
+
+use Illuminate\Database\Eloquent\Model;
+
+class User extends Model {
+
+    protected $table = 'user';
+    
+    public function post(){
+        
+    }
+
+}
